6603e4e8664639fb99d4d182a03acf2a421b781d
Arhip13 <kakashihatake6hokagegygg@gmail.com>
2025-02-17 23:49:25 +0200

    created search input, so project go to end

--- components/GetButton.jsx
+++ components/GetButton.jsx
@@ -5,6 +5,7 @@ import { useAppStore } from "../store/store";
 import { useRouter } from "next/router";
 import { BASE_URL } from "../utils/constants";
 import { getIdFromKey, getRandom } from "../utils/common";
+import Search from "./Search";
 
 const GetButton = ({ text = "Get a movie", cn = "" }) => {
   const router = useRouter();
@@ -29,9 +30,13 @@ const GetButton = ({ text = "Get a movie", cn = "" }) => {
   };
 
   return (
-    <div className={`update ${cn} ${isLoading ? "disabled" : ""}`} onClick={getMovie}>
+    <div className="actions">
+          <div className={`update ${cn} ${isLoading ? "disabled" : ""}`} onClick={getMovie}>
       <Image className="icon" src={update} alt="Refresh" width={14} height={14} />
       <span>{text}</span>
+    </div>
+
+    {router.pathname !== '/' && <Search />}
     </div>
   );
 };
